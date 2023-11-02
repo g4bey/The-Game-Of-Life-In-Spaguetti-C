907eb9beb03178ada23cc4828b6630a74a234b95
Gabey <123382040+onetrickwolfy@users.noreply.github.com>
2023-11-02 03:18:14 +0100

    Fix my crap lol

--- main.c
+++ main.c
@@ -2,29 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int init(SDL_Window *window, SDL_Renderer* renderer) {
-	// init SDL
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
-		fprintf(stderr, "Error SDL_Init : %s", SDL_GetError());
-		return EXIT_FAILURE;
-	}
-
-	// creating window and renderer
-	if(SDL_CreateWindowAndRenderer( 600 , 600 , SDL_WINDOW_RESIZABLE,&window,&renderer)<0)
-	{
-		printf("Error SDL_CreateWindowAndRenderer : %s",SDL_GetError());
-		return EXIT_FAILURE;
-	}
-
-}
-
-void quit(SDL_Window *window, SDL_Renderer* renderer) {
-	SDL_Delay(3000); 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit(); 
-}
-
 void set_color(SDL_Renderer *renderer, const SDL_Color color) {
 	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 }
@@ -34,7 +11,18 @@ int main(int argc, char *argv[])
 	SDL_Window *window; 
 	SDL_Renderer* renderer;
 
-	if (init(window, renderer) == 1) return EXIT_FAILURE;
+	// init SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
+		fprintf(stderr, "Error SDL_Init : %s", SDL_GetError());
+		return 0;
+	}
+
+	// creating window and renderer
+	if(SDL_CreateWindowAndRenderer( 600 , 600 , SDL_WINDOW_RESIZABLE, &window, &renderer)<0)
+	{
+		printf("Error SDL_CreateWindowAndRenderer : %s",SDL_GetError());
+		return 0;
+	}
 
 	const SDL_Color cell_color = {255, 255, 255, 255};
 	const SDL_Color grid_color = {44, 44, 44, 255};
@@ -43,9 +31,12 @@ int main(int argc, char *argv[])
 
 	set_color(renderer, cell_color);
 	SDL_RenderDrawLine(renderer, 4,100,30,100);
-	SDL_RenderPresent(renderer);
+	SDL_RenderPresent(renderer);  
 	
-	quit(window, renderer);
+	SDL_Delay(3000); 
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit(); 
 	return EXIT_SUCCESS;
 }
 
