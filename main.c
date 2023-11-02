e3f284c41fb63dba96fb019c0208a40559bed621
Gabey <123382040+onetrickwolfy@users.noreply.github.com>
2023-11-02 02:12:34 +0100

    Cleanup + rendering a line

--- main.c
+++ main.c
@@ -2,11 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	SDL_Window *window; 
-	SDL_Renderer* renderer;
-	
+int init(SDL_Window *window, SDL_Renderer* renderer) {
 	// init SDL
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
 		fprintf(stderr, "Error SDL_Init : %s", SDL_GetError());
@@ -19,10 +15,37 @@ int main(int argc, char *argv[])
 		printf("Error SDL_CreateWindowAndRenderer : %s",SDL_GetError());
 		return EXIT_FAILURE;
 	}
-	
+
+}
+
+void quit(SDL_Window *window, SDL_Renderer* renderer) {
 	SDL_Delay(3000); 
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
-	SDL_Quit();   
+	SDL_Quit(); 
+}
+
+void set_color(SDL_Renderer *renderer, const SDL_Color color) {
+	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+}
+
+int main(int argc, char *argv[])
+{
+	SDL_Window *window; 
+	SDL_Renderer* renderer;
+
+	if (init(window, renderer) == 1) return EXIT_FAILURE;
+
+	const SDL_Color cell_color = {255, 255, 255, 255};
+	const SDL_Color grid_color = {44, 44, 44, 255};
+
+	SDL_SetWindowTitle(window, "Gabey's game of life.");
+
+	set_color(renderer, cell_color);
+	SDL_RenderDrawLine(renderer, 4,100,30,100);
+	SDL_RenderPresent(renderer);
+	
+	quit(window, renderer);
 	return EXIT_SUCCESS;
 }
 
