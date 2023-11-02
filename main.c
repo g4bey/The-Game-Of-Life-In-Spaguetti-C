55bc4d269c9a688db50e7d84fb00464dcb0e1d89
Gabey <123382040+onetrickwolfy@users.noreply.github.com>
2023-11-02 01:40:07 +0100

    Set-up SDL

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	SDL_Window *window; 
+	SDL_Renderer* renderer;
+	
+	// init SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
+		fprintf(stderr, "Error SDL_Init : %s", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	// creating window and renderer
+	if(SDL_CreateWindowAndRenderer( 600 , 600 , SDL_WINDOW_RESIZABLE,&window,&renderer)<0)
+	{
+		printf("Erreur lors de la creation d'un renderer : %s",SDL_GetError());
+		return EXIT_FAILURE;
+	}
+	
+	SDL_Delay(3000); 
+	SDL_DestroyWindow(window);
+	SDL_Quit();   
+	return EXIT_SUCCESS;
+}
+
