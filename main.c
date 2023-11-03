55b1333d70047a709ec61d44a0cc00eaf1d3a852
Gabey <123382040+onetrickwolfy@users.noreply.github.com>
2023-11-03 01:18:48 +0100

    Drawing a grid

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int init() {
 	}
 
 	// creating window and renderer
-	if(SDL_CreateWindowAndRenderer( 600 , 600 , SDL_WINDOW_RESIZABLE, &window, &renderer)<0)
+	if(SDL_CreateWindowAndRenderer( 600 , 600 , SDL_WINDOW_RESIZABLE | SDL_RENDERER_ACCELERATED, &window, &renderer)<0)
 	{
 		printf("Error SDL_CreateWindowAndRenderer : %s",SDL_GetError());
 		return 0;
@@ -23,18 +23,32 @@ int init() {
 }
 
 void quit() {
-	SDL_Delay(3000); 
+	SDL_Delay(16000); 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit(); 
 }
 
-void set_color(SDL_Renderer *renderer, const SDL_Color color) {
+void set_color(const SDL_Color color) {
 	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+}
+
+void draw_grid(int cell_size, int window_width, int window_height, SDL_Color color) {
+	set_color(color);
+	for (int y = 0; y < window_width; y += cell_size) {
+            SDL_RenderDrawLine(renderer, 0, y, window_width, y);
+			SDL_RenderDrawLine(renderer, y, 0, y, window_height);
+    }
+	SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char *argv[])
 {
+    // + 1 so that the last grid lines fit in the screen.
+    int window_width = 1280;
+    int window_height = 720;
+	int cell_size = 20;
+
 	if (init() != 1) return EXIT_FAILURE;
 
 	const SDL_Color cell_color = {255, 255, 255, 255};
@@ -42,8 +56,12 @@ int main(int argc, char *argv[])
 
 	SDL_SetWindowTitle(window, "Gabey's game of life.");
 
-	set_color(renderer, cell_color);
+	set_color(cell_color);
 	SDL_RenderDrawLine(renderer, 4,100,30,100);
+	SDL_RenderPresent(renderer);  
+
+	draw_grid(cell_size, window_width, window_height, grid_color);
+
 	SDL_RenderPresent(renderer);  
 
 	quit();
