9f04099b9d3d0f4697cf278b0f77831e2890c688
Gabey <123382040+onetrickwolfy@users.noreply.github.com>
2023-11-09 05:03:40 +0100

    Quick push... learning textures

--- main.c
+++ main.c
@@ -4,8 +4,16 @@
 
 SDL_Window *window; 
 SDL_Renderer* renderer;
+SDL_Texture * grid;
 
-int init() {
+const int cell_size = 15;
+const int window_width = 1280 - 1280 % 25;
+const int window_height = 720 - 720 % 25;
+
+SDL_Color red = {255, 0, 0, 255};
+SDL_Color alpha = {0, 0, 0, 0};
+
+int init_window() {
 	// init SDL
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
 		fprintf(stderr, "Error SDL_Init : %s", SDL_GetError());
@@ -13,58 +21,90 @@ int init() {
 	}
 
 	// creating window and renderer
-	if(SDL_CreateWindowAndRenderer( 600 , 600 , SDL_WINDOW_RESIZABLE | SDL_RENDERER_ACCELERATED, &window, &renderer)<0)
+	if(SDL_CreateWindowAndRenderer( window_width , window_height , SDL_WINDOW_RESIZABLE, &window, &renderer)<0)
+	{
+		printf("Error SDL_CreateWindowAndRenderer : %s",SDL_GetError());
+		return 0;
+	}
+	
+	if(SDL_CreateWindowAndRenderer( window_width , window_height , SDL_WINDOW_RESIZABLE, &window, &renderer)<0)
 	{
 		printf("Error SDL_CreateWindowAndRenderer : %s",SDL_GetError());
 		return 0;
 	}
 
+
 	return 1;
 }
 
-void quit() {
-	SDL_Delay(16000); 
+void set_color(SDL_Color * color) {
+	if (color == NULL) {
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	} else {
+		SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, color->a);
+	}
+}
+
+int  init_grid_texture(SDL_Color * color) {
+		grid = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET , window_width, window_height);
+	
+	if (grid == NULL) {
+		printf("Error SDL_CreateTexture : %s",SDL_GetError());
+		return 0;
+	}
+	
+	SDL_SetRenderTarget( renderer, grid );
+	set_color(color);
+	for (int x = 0; x < window_width; x += cell_size) {
+		SDL_RenderDrawLine(renderer, x, 0,  x,  window_height);
+	}
+	
+	for (int y = 0; y < window_height; y += cell_size) {
+		SDL_RenderDrawLine(renderer, 0,  y,  window_width,  y);
+	}
+	
+	SDL_SetRenderTarget( renderer, NULL );
+	
+	return 1;
+}
+
+// TODO... 
+// https://stackoverflow.com/questions/62379457/how-to-draw-a-grid-of-pixels-fast-with-sdl2
+// Try the solution tomorrow.
+// https://fr.wikibooks.org/wiki/Programmation_avec_la_SDL/Les_textures
+// https://github.com/IsakBrendestam/GridTemplate/blob/main/main.c
+// https://github.com/catsocks/sdl-grid
+// https://www.youtube.com/watch?v=75yEVx8iXYQ < from suirface
+// https://www.youtube.com/watch?v=75yEVx8iXYQ
+// https://fr.wikibooks.org/wiki/Programmation_avec_la_SDL/Les_textures
+// https://www.youtube.com/watch?v=XfZ6WrV5Z7Y&t=2700s
+// fr https://www.youtube.com/watch?v=IF99v5_uHEY
+// https://zestedesavoir.com/tutoriels/1014/utiliser-la-sdl-en-langage-c/modification-pixels-par-pixels/
+// https://benedicthenshaw.com/soft_render_sdl2.html
+int main(int argc, char *argv[])
+{
+	
+	if (init_window() != 1) return EXIT_FAILURE;	
+	if (init_grid_texture(&red) != 1) return EXIT_FAILURE;
+	SDL_SetWindowTitle(window, "Gabey's game of life.");
+	
+	SDL_RenderCopy(renderer, grid, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
+	SDL_Event event;
+	while (1)
+	{
+		SDL_WaitEvent(&event);
+		if (event.type == SDL_QUIT) {
+			break;
+		}
+		SDL_Delay(10);
+	}
+
+	SDL_DestroyTexture(grid);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit(); 
-}
-
-void set_color(const SDL_Color color) {
-	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-}
-
-void draw_grid(int cell_size, int window_width, int window_height, SDL_Color color) {
-	set_color(color);
-	for (int y = 0; y < window_width; y += cell_size) {
-            SDL_RenderDrawLine(renderer, 0, y, window_width, y);
-			SDL_RenderDrawLine(renderer, y, 0, y, window_height);
-    }
-	SDL_RenderPresent(renderer);
-}
-
-int main(int argc, char *argv[])
-{
-    // + 1 so that the last grid lines fit in the screen.
-    int window_width = 1280;
-    int window_height = 720;
-	int cell_size = 20;
-
-	if (init() != 1) return EXIT_FAILURE;
-
-	const SDL_Color cell_color = {255, 255, 255, 255};
-	const SDL_Color grid_color = {44, 44, 44, 255};
-
-	SDL_SetWindowTitle(window, "Gabey's game of life.");
-
-	set_color(cell_color);
-	SDL_RenderDrawLine(renderer, 4,100,30,100);
-	SDL_RenderPresent(renderer);  
-
-	draw_grid(cell_size, window_width, window_height, grid_color);
-
-	SDL_RenderPresent(renderer);  
-
-	quit();
 	return EXIT_SUCCESS;
 }
 
